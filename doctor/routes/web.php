d62148dc456fc6fad49e2ba631a3c67b53f42be0
Reena1995 <riyavalekar19@gmail.com>
2022-08-25 15:18:01 +0530

    yes

--- doctor/routes/web.php
+++ doctor/routes/web.php
@@ -25,7 +25,7 @@ use App\Http\Controllers\specialist_controller;
 | contains the "web" middleware group. Now create something great!
 |
 */
-///////////////patient
+///////////////patientbhbjhbjb
 Route::get('/', function () {
     return view('welcome');
 });
