c6d826926a1efda57cf72bf641195f304417772e
Reena1995 <riyavalekar19@gmail.com>
2022-12-16 18:50:31 +0530

    abstrac final

--- technotery/oopsprac/abstract.php
+++ technotery/oopsprac/abstract.php
@@ -2,6 +2,28 @@
 1)class name abstrac jene banavanu hoi e
 2)abstrac class no object na bane.
 3)abstrac class ni property use karva derived class ma inherit karvu pade.
-4)minimum ek method create karvani pan implement nai karvani.implement function nu derived class ma karvanu. -->
+4)minimum ek method create karvani pan implement nai karvani.implement function nu derived class ma karvanu. 
+and argument same no of pass karvani-->
 
-ssssssssssssssssssssssssssss
+<?php
+
+    abstract class employee{
+
+        public $name;
+
+        abstract protected function calc($a,$b);
+    }
+
+    class manager extends employee{
+
+        public function calc($c,$d)
+        {
+            echo $c + $d;
+        }
+
+
+    }
+    $test=new manager();
+    $test->calc(10,20);
+
+?>
