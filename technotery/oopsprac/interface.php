b52c15db88354dc2774031266a2f45b6acc9fce2
Reena1995 <riyavalekar19@gmail.com>
2022-12-16 19:00:38 +0530

    interface

---
+++ technotery/oopsprac/interface.php
@@ -0,0 +1,10 @@
+<!-- php support only single inheritance if multiple you can use interface or traits.
+Rules of interface
+1) 3 class che A,B,C aa ni property and method class D ma use karvani che to 
+    A,b,c interface class kehvay and D is derived class kehvay
+2)interface class ma property assign nai karvani
+3)interface no object na bane
+4)same no of argument pass karvani.
+5)function or method badha interface class ma declare karishu pan implement nai kariye.
+6)function ni agad public lakhi sakay .but private nai lakhvanu.    
+7)implement keyword use thai -->
