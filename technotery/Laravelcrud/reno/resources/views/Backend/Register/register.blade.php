77866e73572b504557ce279da28d465f67e806e9
Reena1995 <riyavalekar19@gmail.com>
2022-12-28 19:15:59 +0530

    rrr

--- technotery/Laravelcrud/reno/resources/views/Backend/Register/register.blade.php
+++ technotery/Laravelcrud/reno/resources/views/Backend/Register/register.blade.php
@@ -1,12 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h3>zdfbxdf</h3>
-</body>
-</html>
+@extends('Backend.Partial.master')
+@section('content')
+	
+
+		<div class="typo-agile">
+			<form id="category_form" action="" method="post" enctype="multipart/form-data">
+			@csrf
+  				<div class="row">
+							<div class="col-12">
+								<h2 class="w3ls_head">Category</h2>
+							</div>
+						<div class="col-lg-6">
+							<div class="input-main">
+								<label class="form-label" for="form3Example1">Category Name</label>
+								<div class="form-outline">
+									<input type="text" id="form3Example1" name="name" class="form-control" />
+									@if ($errors->has('name'))
+        								<span class="error">{{ $errors->first('name') }}</span>
+       								@endif
+								</div>
+							</div>
+							
+						</div>
+						<div class="col-lg-6">
+							<div class="input-main">
+								<label class="form-label" for="form3Example1">Image</label>
+								<div class="form-outline">
+									<input type="file" id="form3Example1" name ="image"  class="form-control" />
+									@if ($errors->has('image'))
+            							<span class="error">{{ $errors->first('image') }}</span>
+       								@endif
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-12">
+							<div class="submit-btn-main">
+								<button type="submit" class="btn btn-primary btn-block mb-4"   value="submit" name="submit" id="btnsubmit">Sign up</button>
+							</div>
+
+						</div>
+				</div>		
+
+			</form>
+		</div>
+
+
+@endsection
